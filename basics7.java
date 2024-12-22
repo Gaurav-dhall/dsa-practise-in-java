4cb843ae5060a73c0a7f6ee8e96bca7e14db4485
gaurav <gauravdhall35@gmail.com>
2024-12-22 15:58:20 +0530

    basics  practise

--- basics7.java
+++ basics7.java
@@ -1,8 +1,31 @@
 public class basics7 {
     public static void main(String[] args) {
-        //logical operators
-        /*
-         &&
-         */
+       int day=45;//1-monday 2-tuesday.....7-sunday
+
+       switch (day) {
+        case 1:System.out.println("Today is monday");
+            
+            break;
+        case 2:System.out.println("Today is tuesday");
+            
+            break;
+        case 3:System.out.println("Today is wednesday");
+            
+            break;
+        case 4:System.out.println("Today is thursday");
+            
+            break;
+        case 5:System.out.println("Today is friday");
+            
+            break;
+        case 6:System.out.println("Today is saturday");
+            
+            break;
+        
+
+       
+        default:System.out.println("Today is sunday");
+            break;
+       }
     }
 }
---
+++ basics8.java
@@ -0,0 +1,15 @@
+public class basics8 {
+    public static void main(String[] args) {
+        //loops
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        int i=1;
+
+        while(i<=100){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
